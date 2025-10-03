aa70fb772223409df28b47cae38db7dda7301b06
Nischal <tamangnischal2018@gmail.com>
2025-10-03 18:05:10 +0545

    feat:creating routes and added shadcn UI library

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import {ReactNode} from 'react'
+
+const AuthLayout = ({children}: {children : ReactNode}) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default AuthLayout
